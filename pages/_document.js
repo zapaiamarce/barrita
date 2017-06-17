0189663e38aacab45cb5c3f9ec234de6c9a68bf8
Marcelo Zapaia <mzapaia@ints-MacBook-Pro.local>
2017-06-17 12:54:45 -0300

    styled components tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components'
+
 
 export default class MyDocument extends Document {
 	render() {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+
 		return (
 			<html style={{ background: '#EEE', color: '#444' }}>
 				<Head>
@@ -16,10 +22,11 @@ export default class MyDocument extends Document {
 						rel="stylesheet"
 						href="https://code.getmdl.io/1.3.0/material.deep_purple-blue.min.css"
 					/>
+          {styleTags}
 					<title>Todo App</title>
 				</Head>
 				<body>
-          <Main />
+          {main}
 					<NextScript />
 					<script defer src="https://code.getmdl.io/1.3.0/material.min.js" />
 				</body>
