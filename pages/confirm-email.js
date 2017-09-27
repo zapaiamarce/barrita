f8501e3c8893c6edcd97389540c509f8180bcb8e
Marcelo Zapaia <mzapaia@ints-MacBook-Pro.local>
2017-09-26 22:14:53 -0300

    add files

---
+++ pages/confirm-email.js
@@ -0,0 +1,4 @@
+
+export default (props) => (
+  <div>Email enviado</div>
+);
---
+++ pages/letsgo.js
@@ -0,0 +1,7 @@
+import SignupIn from  '../components/SignupIn/withData';
+import withData from '../lib/withData';
+
+
+export default withData((props) => (
+  <SignupIn></SignupIn>
+));
